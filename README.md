afb29de01e358d9c2630ae8fc19b065dc216818d
ThisFunctionalTom <tomas.leko@gmail.com>
2020-11-13 17:55:05 +0100

    move to webpack

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HTML2Feliz
 
 A small tool to generate [Feliz](https://zaid-ajaj.github.io/Feliz/) code from given HTML input.
+
+Here is a [Demo](https://thisfunctionaltom.github.io/Html2Feliz/)
--- src/Html2Feliz.fs
+++ src/Html2Feliz.fs
@@ -1,45 +1,43 @@
 module Html2Feliz
 
 open System
-open FSharp.Data
 
-type HtmlAttribute = {
-    Name: string
-    Value: string
-}
+type HtmlAttribute = { Name: string; Value: string }
 
-type HtmlNode = {
-    Name: string
-    Attributes: HtmlAttribute list
-    Elements: HtmlNode list
-    DirectInnerText: string option
-}
+type HtmlNode =
+    { Name: string
+      Attributes: HtmlAttribute list
+      Elements: HtmlNode list
+      DirectInnerText: string option }
 
-type HtmlDocument = {
-    Elements: HtmlNode list
-}
+type HtmlDocument = { Elements: HtmlNode list }
 
 let (|Text|SingleTextChild|Attributes|Children|Complex|) (node: HtmlNode) =
     let attrs = node.Attributes
     let hasAttrs = not attrs.IsEmpty
     let children = node.Elements
     let hasChildren = not children.IsEmpty
-    let hasSingleTextChild = not hasAttrs && not hasChildren && Option.isSome node.DirectInnerText
+
+    let hasSingleTextChild =
+        not hasAttrs
+        && not hasChildren
+        && Option.isSome node.DirectInnerText
+
     let name = node.Name
 
-    if hasSingleTextChild then 
-        SingleTextChild (name, node.DirectInnerText)
+    if hasSingleTextChild then
+        SingleTextChild(name, node.DirectInnerText)
     else
         match hasChildren, hasAttrs with
-        | false, false -> Text (node.DirectInnerText)
-        | false, true -> Attributes (name, attrs)
-        | true, false -> Children (name, children)
-        | true, true -> Complex (name, attrs, children)
+        | false, false -> Text(node.DirectInnerText)
+        | false, true -> Attributes(name, attrs)
+        | true, false -> Children(name, children)
+        | true, true -> Complex(name, attrs, children)
 
 let formatAttribute indent level (attr: HtmlAttribute) =
-    let indentStr = String(' ', indent*level)
+    let indentStr = String(' ', indent * level)
     if attr.Name = "class" then
-        let classes = attr.Value.Split(' ') 
+        let classes = attr.Value.Split(' ')
         match classes with
         | [| single |] -> $"{indentStr}prop.className \"{single}\""
         | multi ->
@@ -47,14 +45,16 @@ let formatAttribute indent level (attr: HtmlAttribute) =
                 multi
                 |> Array.map (sprintf "\"%s\"")
                 |> String.concat "; "
+
             sprintf $"{indentStr}prop.className [ {classNames} ]"
     else
         $"{indentStr}prop.{attr.Name} \"{attr.Value}\""
 
 let rec formatNode indent level (node: HtmlNode) =
-    let line level text = $"{String(' ', indent*level)}{text}"
+    let line level text = $"{String(' ', indent * level)}{text}"
+
     let nodeBlock name content =
-        seq { 
+        seq {
             line level $"Html.{name} ["
             yield! content
             line level "]"
@@ -62,27 +62,31 @@ let rec formatNode indent level (node: HtmlNode) =
 
     seq {
         match node with
-        | Text text -> 
-            line level $"Html.text \"{text}\""
-        | SingleTextChild (node, text) -> 
-            line level $"Html.{node} \"{text}\""
-        | Attributes (name, attrs) -> 
-            yield! nodeBlock name [ 
-                for attr in attrs do 
-                    formatAttribute indent (level+1) attr ]
+        | Text text -> line level $"Html.text \"{text}\""
+        | SingleTextChild (node, text) -> line level $"Html.{node} \"{text}\""
+        | Attributes (name, attrs) ->
+            yield!
+                nodeBlock
+                    name
+                    [ for attr in attrs do
+                        formatAttribute indent (level + 1) attr ]
         | Children (name, children) ->
-            yield! nodeBlock name [ 
-                for child in children do 
-                    yield! formatNode indent (level+1) child ]
+            yield!
+                nodeBlock
+                    name
+                    [ for child in children do
+                        yield! formatNode indent (level + 1) child ]
         | Complex (name, attrs, children) ->
-            yield! nodeBlock name [ 
-                for attr in attrs do 
-                    formatAttribute indent (level+1) attr
-                line (level+1) "prop.children ["
-                for child in children do 
-                    yield! formatNode indent (level+2) child
-                line (level+1) "]" ]
-}
+            yield!
+                nodeBlock
+                    name
+                    [ for attr in attrs do
+                        formatAttribute indent (level + 1) attr
+                      line (level + 1) "prop.children ["
+                      for child in children do
+                          yield! formatNode indent (level + 2) child
+                      line (level + 1) "]" ]
+    }
 
 let formatDocument indent (html: HtmlDocument) =
     seq {
@@ -92,43 +96,51 @@ let formatDocument indent (html: HtmlDocument) =
 
 open Fable.Core
 open Fable.Core.JsInterop
-open Fable.Core.DynamicExtensions
 
 [<Emit("Object.entries($0)")>]
-let objectEntries (jsObj: obj) : (string*string) array = jsNative 
+let objectEntries (jsObj: obj): (string * string) array = jsNative
 
-let parse (htmlString: string) : HtmlDocument =
+let parse (htmlString: string): HtmlDocument =
     let handler = createEmpty<Htmlparser2.Handler>
     let mutable nodes = List.empty
     let mutable current = List.empty
 
-    handler.onopentag <- fun (name : string) (attributes: obj) -> 
-        printfn $"Attributes: {attributes}"
-        let attrs = 
-            objectEntries attributes
-            |> List.ofArray
-            |> List.map (fun (name, value) -> { Name = name; Value = value })
+    handler.onopentag <-
+        fun (name: string) (attributes: obj) ->
+            let attrs =
+                objectEntries attributes
+                |> List.ofArray
+                |> List.map (fun (name, value) -> { Name = name; Value = value })
 
-        printfn $"Attrs: {attrs}"
-        current <- {
-            Name = name
-            Attributes = attrs
-            Elements = List.empty
-            DirectInnerText = None
-        } :: current
-    
-    handler.ontext <- fun text ->
-        match current with
-        | node :: parents ->
-            current <- { node with DirectInnerText = Some text } :: parents
-        | _ -> ()
+            current <-
+                { Name = name
+                  Attributes = attrs
+                  Elements = List.empty
+                  DirectInnerText = None }
+                :: current
 
-    handler.onclosetag <- fun (name : string) -> 
-        match current with
-        | node :: parent :: parents ->
-            current <- { parent with Elements = parent.Elements @ [node] } :: parents
-        | _ -> ()
-    
-    let parser = Htmlparser2.exports.Parser.Create(handler)
-    parser.write(htmlString)
+    handler.ontext <-
+        fun text ->
+            match current with
+            | node :: parents ->
+                current <-
+                    { node with
+                          DirectInnerText = Some text }
+                    :: parents
+            | _ -> ()
+
+    handler.onclosetag <-
+        fun (name: string) ->
+            match current with
+            | node :: parent :: parents ->
+                current <-
+                    { parent with
+                          Elements = parent.Elements @ [ node ] }
+                    :: parents
+            | _ -> ()
+
+    let parser =
+        Htmlparser2.exports.Parser.Create(handler)
+
+    parser.write (htmlString)
     { Elements = current }
